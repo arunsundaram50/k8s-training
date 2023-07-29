30d75939a9abd20a6626ce87b3df00c865b7b9cd
Arun Sundaram <arun.sundaram@redhorsecorp.com>
2023-07-29 15:40:54 -0400

    updatws

--- dd2/main.py
+++ dd2/main.py
@@ -1,12 +1,15 @@
-import fastapi
+import fastapi, os, uvicorn, nest_asyncio, random
 
 app = fastapi.FastAPI()
-
+PORT = os.environ.get('PORT', '8001')
+random_number = random.randint(1, 100)
 
 @app.get("/hello")
 def say_hello():
   return {
-    "message": "Hello, world"
+    "message": f"Hello, world {random_number}"
   }
 
+nest_asyncio.apply()
+uvicorn.run("main:app", host="0.0.0.0", port=PORT, log_level="error")
 
