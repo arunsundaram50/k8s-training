95055ab655d0ef595d775559f230e2c808015542
arunsundaramco <arunsundaramco@gmail.com>
2023-08-04 21:55:46 -0400

    updates

---
+++ dd5b/upper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+s = sys.argv[1].upper()
+
+print(s)
+
---
+++ dd6/upper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+for i in range(1, 1000):
+  s = sys.argv[1].upper()
+  print(f'{i}: {s}')
+
